41925d6a3eca52ff5ab0ec9d38a71516bea11333
Chris Lail <chrislail56@gmail.com>
2019-09-10 20:55:07 -0400

    Adding react frontend

---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from "css/Header.css";
+
+function Header() {
+    return (
+        <div className="Header">
+            <h1>Arc</h1>
+        </div>
+    );
+}
+
+export default Header;
