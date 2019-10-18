3bcfdc56a8d74d307c39406b45b242a5388de8c9
Chris Lail <chrislail56@gmail.com>
2019-10-17 20:17:23 -0400

    Revert "Using electron-react-boilerplate to create starting UI"
    This reverts commit 69a10bdeb3aae65198a5e8c3f41a3ca4df7e361b.

---
+++ client/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./css/Header.css";
+
+function Header() {
+    return (
+        <header className="Header">
+            <h1>Ether</h1>
+            <h4>VXML Generator</h4>
+        </header>
+    );
+}
+
+export default Header;
