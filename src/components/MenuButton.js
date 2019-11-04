6af82e713d2118a6ba609a1dcc84fd0465781892
Chris Lail <chrislail56@gmail.com>
2019-11-04 18:37:49 -0500

    Refactored all components to functional
    Also renamed containers folder to pages

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,22 +1,20 @@
-import React, {Component} from 'react';
-import {Divider, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
+import React from 'react';
+import { Divider, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 
-class MenuButton extends Component {
-    render() {
-        return (
-            <div>
-                <ListItem button onClick={this.props.onClick}>
-                    <ListItemIcon>
-                        {this.props.icon}
-                    </ListItemIcon>
-                    <ListItemText>
-                        {this.props.text}
-                    </ListItemText>
-                </ListItem>
-                <Divider/>
-            </div>
-        );
-    }
-}
+const MenuButton = (props) => {
+    return (
+        <div>
+            <ListItem button onClick={props.onClick}>
+                <ListItemIcon>
+                    {props.icon}
+                </ListItemIcon>
+                <ListItemText>
+                    {props.text}
+                </ListItemText>
+            </ListItem>
+            <Divider/>
+        </div>
+    );
+};
 
 export default MenuButton;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from "./containers/App"
+import App from "./pages/App"
 
 ReactDOM.render(<App />, document.getElementById('root'));
