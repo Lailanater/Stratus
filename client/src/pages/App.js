be78e14368d46837868e703673912888339529aa
Chris Lail <chrislail56@gmail.com>
2019-11-27 16:08:25 -0500

    Adding local save

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -4,9 +4,14 @@ import { Provider } from "react-redux";
 import store from "../redux/store";
 
 import Theming from "./Theming";
+import { saveState } from "../localStorage";
 
 function App() {
-    store.subscribe(() => console.log(store.getState()));
+    store.subscribe(() => {
+        const currentState = store.getState();
+        console.log(currentState);
+        saveState(currentState);
+    });
 
     return (
         <Provider store={store}>
