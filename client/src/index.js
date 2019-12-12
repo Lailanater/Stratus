9557613eeeb1ba47f0397251f7e258a5c50a5dac
Chris <chrislail56@gmail.com>
2019-12-12 14:44:34 -0600

    Undoing test

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,3 @@ import './css/index.css';
 import App from './pages/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-function test(  hello   ) { var example = 2
-print(example)}
