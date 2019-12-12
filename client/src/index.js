fde83a89f607629b87b86e5364f3f652943a60dd
Chris <chrislail56@gmail.com>
2019-12-12 14:42:04 -0600

    test

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,6 @@ import './css/index.css';
 import App from './pages/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+function test(  hello   ) { var example = 2
+print(example)}
