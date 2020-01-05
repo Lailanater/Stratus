717c3d25a8154a499eba1f560512c83fc49d44cd
Chris Lail <chrislail56@gmail.com>
2020-01-05 15:01:07 -0500

    Fixing button styling
    Does not seem to work with styled-components

--- client/src/components/FileExistsDialog.js
+++ client/src/components/FileExistsDialog.js
@@ -11,17 +11,6 @@ const DialogContainer = styled.div`
   padding: 20px;
 `;
 
-const YesButton = styled(Button)`
-  padding: 10px;
-  background-color: #4caf50;
-`;
-
-const NoButton = styled(Button)`
-  margin-left: 20px;
-  padding: 10px;
-  background-color: #f44336;
-`;
-
 const FileExistsDialog = props => {
   return (
     <Dialog open={props.open}>
@@ -33,8 +22,22 @@ const FileExistsDialog = props => {
           Are you sure you want to overwrite it?
         </DialogContentText>
         <div className="centerText">
-          <YesButton onClick={props.yesOnClick}>Yes</YesButton>
-          <NoButton onClick={props.noOnClick}>No</NoButton>
+          <Button
+            style={{ padding: '10px', backgroundColor: '#4caf50' }}
+            onClick={props.yesOnClick}
+          >
+            Yes
+          </Button>
+          <Button
+            style={{
+              marginLeft: '20px',
+              padding: '10px',
+              backgroundColor: '#f44336'
+            }}
+            onClick={props.noOnClick}
+          >
+            No
+          </Button>
         </div>
       </DialogContainer>
     </Dialog>
